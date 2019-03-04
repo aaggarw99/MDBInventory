188b6cfa3f6cb377041a129683544b98306c453e
aaggarw99 <aaggarw99@gmail.com>
2019-03-04 01:44:19 -0800

    added one missing line to xml

--- app/src/main/java/com/example/mdbinventory/MainActivity.java
+++ app/src/main/java/com/example/mdbinventory/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -20,10 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
         Purchase dummy = new Purchase("Alcohol", 5, "Something to consume", "Target", "10/15/1999");
         Purchase dummy2 = new Purchase("Napkins", 1, "A piece of paper", "Target", "02/19/2019");
+        Purchase dummy3 = new Purchase("Forks", 1, "Eating utensil", "Target", "12/31/2018");
 
         purchases = new ArrayList<>();
         purchases.add(dummy);
         purchases.add(dummy2);
+        purchases.add(dummy3);
+
+        Log.i("Size", Integer.toString(purchases.size()));
+        Log.i("Purchases content", purchases.get(0).getName());
+        Log.i("Purchases content", purchases.get(1).getName());
+        Log.i("Purchases content", purchases.get(2).getName());
+
 
         listOfPurchases = findViewById(R.id.rv);
         listOfPurchases.setLayoutManager(new LinearLayoutManager(this));
